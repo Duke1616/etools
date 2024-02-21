811e3fe54fea422b7121d4da07d771cd6a9ca616
luankz <17682333171@163.com>
2024-02-21 10:18:15 +0800

    添加fixer consumer

---
+++ migrator/events/fixer/consumer.go
@@ -0,0 +1,47 @@
+package fixer
+
+import (
+	"context"
+	"errors"
+	"github.com/Duke1616/etools/kafka/saramax"
+	"github.com/Duke1616/etools/logger"
+	"github.com/Duke1616/etools/migrator"
+	"github.com/Duke1616/etools/migrator/events"
+	"github.com/Duke1616/etools/migrator/fixer"
+	"github.com/IBM/sarama"
+	"time"
+)
+
+type Consumer[T migrator.Entity] struct {
+	client   sarama.Client
+	l        logger.Logger
+	srcFirst *fixer.OverrideFixer[T]
+	dstFirst *fixer.OverrideFixer[T]
+	topic    string
+}
+
+func (c *Consumer[T]) Start() error {
+	eg, err := sarama.NewConsumerGroupFromClient("migrator-fix", c.client)
+	if err != nil {
+		return err
+	}
+	go func() {
+		if err := eg.Consume(context.Background(), []string{c.topic},
+			saramax.NewHandler[events.InconsistentEvent](c.Consumer)); err != nil {
+			c.l.Error("退出了消费循环异常", logger.Error(err))
+		}
+	}()
+	return err
+}
+
+func (c *Consumer[T]) Consumer(msg *sarama.ConsumerMessage, evt events.InconsistentEvent) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	switch evt.Direction {
+	case "SRC":
+		return c.srcFirst.Fix(ctx, evt)
+	case "DST":
+		return c.dstFirst.Fix(ctx, evt)
+	}
+	return errors.New("未知的校验方向")
+}
--- migrator/fixer/fixer.go
+++ migrator/fixer/fixer.go
@@ -1,1 +1,37 @@
 package fixer
+
+import (
+	"context"
+	"github.com/Duke1616/etools/migrator"
+	"github.com/Duke1616/etools/migrator/events"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type OverrideFixer[T migrator.Entity] struct {
+	base   *gorm.DB
+	target *gorm.DB
+
+	columns []string
+}
+
+func (o OverrideFixer[T]) Fix(ctx context.Context, evt events.InconsistentEvent) error {
+	switch evt.Type {
+	case events.InconsistentEventTypeNEQ, events.InconsistentEventTypeTargetMissing:
+		var t T
+		err := o.base.WithContext(ctx).Where("id=?", evt.ID).First(&t).Error
+		switch err {
+		case gorm.ErrRecordNotFound:
+			return o.target.Model(&t).Delete("id = ?", evt.ID).Error
+		case nil:
+			return o.target.WithContext(ctx).Clauses(clause.OnConflict{
+				DoUpdates: clause.AssignmentColumns(o.columns),
+			}).Create(&t).Error
+		default:
+			return err
+		}
+	case events.InconsistentEventTypeBaseMissing:
+		return o.target.Model(new(T)).Delete("id = ?", evt.ID).Error
+	}
+	return nil
+}
